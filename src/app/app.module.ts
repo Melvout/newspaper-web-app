a30ebbfe27ea086409364b2c00dda2774a81ad1f
Melvin NAMONT <melvin.namont@gmail.com>
2020-10-07 19:04:43 +0200

    articles-details component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleDetailsComponent } from './components/article-details/article-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleDetailsComponent
   ],
   imports: [
     BrowserModule,
