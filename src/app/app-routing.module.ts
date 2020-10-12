8ef33908ad6a999307e0f8ffd7bc4d06beb2d919
Melvin NAMONT <melvin.namont@gmail.com>
2020-10-12 11:27:19 +0200

    Added login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ArticleListComponent } from './components/article-list/article-list.component';
 import { ArticleEditionComponent } from './components/article-edition/article-edition.component';
 import { ArticleDetailsComponent } from './components/article-details/article-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/articles-list', pathMatch: 'full' },
   { path: 'articles-list', component: ArticleListComponent },
   { path: 'article-edition/:id', component: ArticleEditionComponent },
-  { path: 'article-details/:id', component: ArticleDetailsComponent }
+  { path: 'article-details/:id', component: ArticleDetailsComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
